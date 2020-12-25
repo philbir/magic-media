f2b9500133df8ece80cde80efe5ee5d00cb81544
Philippe Birbaum <tree@gmx.ch>
2020-12-25 08:28:31 +0100

    Improve user feedback un preload

--- src/UI/media-ui/src/components/User/MeLoader.vue
+++ src/UI/media-ui/src/components/User/MeLoader.vue
@@ -2,13 +2,33 @@
   <div>
     <slot v-if="ready"></slot>
     <slot v-if="isRootSlot" name="error"></slot>
+    <v-app v-if="me == null">
+      <div style="width: 100%; height: 60vh">
+        <v-container fill-height fluid>
+          <v-row align="center" justify="center">
+            <v-col class="d-flex justify-center">
+              <div class="text-center">
+                <v-progress-circular
+                  :size="28"
+                  color="blue"
+                  indeterminate
+                ></v-progress-circular>
+                {{ message }}
+              </div>
+            </v-col>
+          </v-row>
+        </v-container>
+      </div>
+    </v-app>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
 export default {
-  data: () => ({}),
+  data: () => ({
+    message: "Authenticating user...",
+  }),
   computed: {
     ...mapState("user", ["me", "error"]),
     ready: function () {
@@ -22,6 +42,20 @@ export default {
     this.$store.dispatch("user/getMe");
   },
   watch: {
+    me: function (newValue) {
+      if (newValue) {
+        this.message = "Loading data...";
+        this.$store.dispatch("person/getAll").then(() => {
+          this.preloaded = true;
+        });
+        this.$store.dispatch("person/getAllGroups").then(() => {
+          this.preloaded = true;
+        });
+        this.$store.dispatch("user/getAll").then(() => {
+          this.preloaded = true;
+        });
+      }
+    },
     error: function (newValue) {
       if (newValue && !this.$route.meta.isRoot) {
         this.$router.push({ name: "Error" });
